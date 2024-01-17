ee17bbf58e897e908d5cb616cfdf3c22496ec2ce
AndreaGul <andreagulino99@gmail.com>
2024-01-17 18:49:03 +0100

    feat:aggiunta trait categoria prodotto

--- index.php
+++ index.php
@@ -3,20 +3,16 @@
 //inizializziamo la classe
 class Product{
   
-  
-  public $img;
-  public $title;
-  public $prize;
+  protected $id;
+  protected $img;
+  protected $title;
+  protected $prize;
   
  
 
-  function __construct($_img,$_title,$_prize)
+  function __construct($_id,$_title,$_prize)
   {
-      
-      
-      
-      
-      $this-> img = $_img;
+      $this-> id = $_id;
       $this-> title = $_title;
       $this-> prize = $_prize;
       
@@ -30,36 +26,46 @@ class Product{
   }
   
    
+  public function getPrize (){
+    return $this->prize;
+  }
 
 
 };
 
 
  class ProductType extends Product{
-  public $prodType;
+  private $prodType;
 
-   function __construct($_prodType,$_img,$_title,$_prize){
-      parent:: __construct($_img,$_title,$_prize);
+  use Category;
+
+   function __construct($_prodType,$_id,$_title,$_prize,$_category,$_categoryIcon){
+      parent:: __construct($_id,$_title,$_prize);
       $this-> prodType =$_prodType;
-  } }
+      $this->setCategory($_category,$_categoryIcon);
+  } 
+
+  public function get_product(){
+
+      return "Categoria:{$this->category} <br> {$this->title}, prezzo {$this->prize}$ Categoria {$this->prodType} per {$this->category} {$this->categoryIcon}";
+    }
+
+}
+
+trait Category{
+  private $category;
+  private $categoryIcon;
+
+  public function setCategory($_category,$_categoryIcon){
+    $this->category=$_category;
+    $this->categoryIcon=$_categoryIcon;
+  }
+
+}
 
  
 
-   class CategoryProduct extends ProductType { 
-    public $category;
 
-    function __construct($_category,$_prodType,$_img,$_title,$_prize)
-    {
-      parent:: __construct($_prodType,$_img,$_title,$_prize);
-      $this-> category = $_category;
-    }
-
-    public function get_product(){
-
-      return "{$this->img} <br> {$this->title}, prezzo {$this->prize}$ Categoria {$this->prodType} per {$this->category}";
-    }
-
-  }
 
 //creaiamo le istanze
 // $crocchettePerGatti = new CategoryProduct('gatti','cibo','imgCrocchetteGatti','Crocchette per gatti','10');
@@ -67,9 +73,9 @@ class Product{
 // $palla = new CategoryProduct('gatti','giochi','imgPalla','Palla','5');
 
 $products = [
-  new CategoryProduct('gatti','cibo','imgCrocchetteGatti','Crocchette per gatti','10'),
-  new CategoryProduct('cani','cucce','imgCucciaGrande','Cuccia grande',0),
-  new CategoryProduct('gatti','giochi','imgPalla','Palla','5'),
+  new ProductType('cibo',100,'Crocchette per gatti','10','Gatti','<i class="fa-solid fa-cat"></i>'),
+  new ProductType('cucce',101,'Cuccia grande',0,'Cani','<i class="fa-solid fa-dog"></i>'),
+  new ProductType('giochi',102,'Palla','5','Gatti','<i class="fa-solid fa-cat"></i>'),
 ];
 
 
@@ -88,16 +94,18 @@ $products = [
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>E-commerce</title>
+
+  <!-- font awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
   <?php foreach($products as $product): ?>
     <div> <?php
-    try {
-       echo  $product->controlPrice($product->prize);
-     } catch (Exception $error){
-       echo $error->getMessage();
-       
-    };
+      try {
+        echo  $product->controlPrice($product->getPrize ());
+      } catch (Exception $error){
+        echo $error->getMessage();
+      };
        echo $product->get_product() ?></div>
     <br>
   <?php endforeach;?>

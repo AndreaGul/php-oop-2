b955b5f59d001fe3217a7bff9467d122acb73ef4
AndreaGul <andreagulino99@gmail.com>
2024-01-17 01:30:40 +0100

    feat:struttura base della classe product

--- index.php
+++ index.php
@@ -1,6 +1,49 @@
 <?php
 
+//inizializziamo la classe
+class Product{
+  public $category;
+  public $prodType;
+  public $img;
+  public $title;
+  public $prize;
+  
+ 
 
+  function __construct($_category,$_prodType,$_img,$_title,$_prize)
+  {
+      
+      $this-> category = $_category;
+      $this-> prodType =$_prodType;
+      $this-> img = $_img;
+      $this-> title = $_title;
+      $this-> prize = $_prize;
+      
+  }
+};
+
+// class Cibo extends Product{
+//   private $category = 'cibo';
+
+//    function __construct($_img,$_title){
+//       parent:: __construct($_category,$_img,$_title,$_prize);
+      
+//   }
+// }
+
+//creaiamo le istanze
+$crocchettePerGatti = new Product('gatto','cibo','imgCrocchetteGatti','Crocchette per gatti','10');
+$cucciaGrande = new Product('cani','cucce','imgCucciaGrande','Palla','16');
+$palla = new Product('gatto','giochi','imgPalla','Palla','5');
+
+
+var_dump($crocchettePerGatti,$cucciaGrande,$palla)
+// class gatto {
+
+// }
+// class gane {
+  
+// }
 
 ?>
 <!DOCTYPE html>
